f6fb4ac9db47d3d0d226cb52d8d9fccdd0794776
ben20262 <ben20262@aim.com>
2020-07-04 10:02:43 -0400

    added dessertsReducer

---
+++ src/reducers/dessertsReducer.js
@@ -0,0 +1,23 @@
+const desserts = [
+    {
+        name: 'Ice Cream',
+        price: 2,
+        blurb: 'The end to an summer day is imcomplete without this tasty treat.'
+    },
+    {
+        name: 'Birthday Cake',
+        price: 10,
+        blurb: 'A dessert for a special occasion please let us know ahead of time for special requests.'
+    }
+]
+
+export default function dessertsReducer(state = desserts, action) {
+    switch (action.type) {
+        case 'ADD_DESSERT':
+            return state
+        case 'DELETE_DESSERT':
+            return state
+        default:
+            return state
+    }
+}
